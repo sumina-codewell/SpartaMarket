c88d3dcdf7085f647fa6f618bcf6a8f37f3766df
sumina-codewell <sumina110423@gmail.com>
2024-04-19 15:52:58 +0900

    accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
 
-# Create your views here.
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect("products:products")
+    else:
+        form = AuthenticationForm()
+    context = {"form": form}
+    return render(request, "accounts/login.html", context)
+
+def logout(request):
+    if request.method == "POST":
+        auth_logout(request)
+    return redirect("products:products")
